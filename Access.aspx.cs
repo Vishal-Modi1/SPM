14308fae4781f4ca420b9a990eeea5125c305179
Vishal-Modi1 <modivishalp@gmail.com>
2016-10-05 14:48:37 +0530

    Added From Local Machine

---
+++ Access.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.OleDb;
+using System.Data;
+
+
+public partial class Access : System.Web.UI.Page
+{
+    OleDbConnection con = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory\Database1.accdb");
+    OleDbCommand cmd;
+    OleDbDataAdapter da;
+  
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        cmd = new OleDbCommand("select * from stud", con);
+       
+        da = new OleDbDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        da.Fill(ds);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        OleDbCommand cmd = new OleDbCommand("insert into stud(sname,city) values ('mahesh','surat')", con);
+        cmd.ExecuteNonQuery();
+        GridView1.DataBind();
+        con.Close();
+        Page_Load(sender, e);
+
+        
+    }
+}
